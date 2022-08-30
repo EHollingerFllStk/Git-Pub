fea73d58951d8dbd3cbe719e5ffd2dc27ecdb0fc
Beth-Hollinger <hollingere15@gmail.com>
2022-08-30 17:27:29 -0400

    Added drinks object

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
+const drinks = require("./models/drinks.js")
+
+// REQUIRE DEPENDENCIES
 const express = require('express');
+//INITIALIZE EXPRESS APP
 const app = express();
 const port = 3000;
+
+// DEFINE ROUTES
 
 app.get("/", (req, res) => {
     res.send("Welcome to Gitpub App!")
 })
 
+//Route for drink DB
 
+app.get("/drinks/",(req, res) => {
+    res.send(drinks)
+})
+
+
+// SET UP LISTEN
 app.listen(port, () => {
     console.log("listening live", port)
 })
